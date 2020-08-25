9b7fb73f821d9bf93be93388a3a5312481bcea36
Cloud User <centos@ip-172-31-28-75.ap-northeast-1.compute.internal>
2020-08-25 12:59:43 +0000

    use loop in delete

--- lib/torb/web.rb
+++ lib/torb/web.rb
@@ -344,7 +344,7 @@ module Torb
       sheet = nil
       reservation_id = nil
       loop do
-        sheet = db.xquery('SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1', event_id, rank).first
+        sheet = db.xquery('SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled = 0 FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1', event_id, rank).first
         halt_with_error 409, 'sold_out' unless sheet
         db.query('BEGIN')
         begin
@@ -373,24 +373,28 @@ module Torb
       sheet = db.xquery('SELECT * FROM sheets WHERE `rank` = ? AND num = ?', rank, num).first
       halt_with_error 404, 'invalid_sheet' unless sheet
 
-      db.query('BEGIN')
-      begin
-        reservation = db.xquery('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id HAVING reserved_at = MIN(reserved_at)', event['id'], sheet['id']).first
-        unless reservation
-          db.query('ROLLBACK')
-          halt_with_error 400, 'not_reserved'
-        end
-        if reservation['user_id'] != user['id']
-          db.query('ROLLBACK')
-          halt_with_error 403, 'not_permitted'
-        end
+      loop do
+        db.query('BEGIN')
+        begin
+          reservation = db.xquery('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled = 0 GROUP BY event_id HAVING reserved_at = MIN(reserved_at)', event['id'], sheet['id']).first
+          unless reservation
+            db.query('ROLLBACK')
+            halt_with_error 400, 'not_reserved'
+          end
+          if reservation['user_id'] != user['id']
+            db.query('ROLLBACK')
+            halt_with_error 403, 'not_permitted'
+          end
 
-        db.xquery('UPDATE reservations SET canceled_at = ?, canceled = 1 WHERE id = ?', Time.now.utc.strftime('%F %T.%6N'), reservation['id'])
-        db.query('COMMIT')
-      rescue => e
-        warn "rollback by: #{e}"
-        db.query('ROLLBACK')
-        halt_with_error
+          db.xquery('UPDATE reservations SET canceled_at = ?, canceled = 1 WHERE id = ?', Time.now.utc.strftime('%F %T.%6N'), reservation['id'])
+          db.query('COMMIT')
+        rescue => e
+          warn "rollback by: #{e}"
+          db.query('ROLLBACK')
+          next#
+          #halt_with_error
+        end
+        break
       end
 
       status 204
